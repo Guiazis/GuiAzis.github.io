5f080080b159f652832c6580535380f5fc01dcd4
Jairo Luckas <52499442+J-Luckas@users.noreply.github.com>
2021-01-04 18:10:49 -0300

    teste

---
+++ GuiAzis.github.io-master/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".grid-imagem").on("mouseover", "div", function(){
+        $(this).find("img").css({"-webkit-filter": "blur(1px)", "filter": "blur(1px)"});
+        $(this).find("p").css("display", "block");
+    });
+    $(".grid-imagem").on("mouseout", "div", function(){
+        $(this).find("img").css({"-webkit-filter": "blur(0px)", "filter": "blur(0px)"});
+        $(this).find("p").css("display", "none");
+    });
+})
+

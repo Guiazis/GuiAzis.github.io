e7586c013dbe062b094e682eeda720e295a52ea8
Jairo Luckas <52499442+J-Luckas@users.noreply.github.com>
2021-01-04 19:31:35 -0300

    Mudança no sobre, preparando para a inserção do JQUERY

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(function(){
+    // Para o index
+
     $(".grid-imagem").on("mouseover", "div", function(){
         $(this).find("img").css({"-webkit-filter": "blur(1px)", "filter": "blur(1px)"});
         $(this).find("p").css("display", "block");
@@ -7,5 +9,9 @@ $(function(){
         $(this).find("img").css({"-webkit-filter": "blur(0px)", "filter": "blur(0px)"});
         $(this).find("p").css("display", "none");
     });
+
+    // Para a página sobre
+
+    
 })
 
